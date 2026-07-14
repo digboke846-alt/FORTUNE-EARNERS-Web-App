28f2587a22ea72d8b94f04cc9565c0cd8bf8f897
digboke846-alt <digboke846@gmail.com>
2026-07-14 18:04:33 +0100

    Update menu.js

--- fortune-earners/js/menu.js
+++ fortune-earners/js/menu.js
@@ -2,41 +2,45 @@ const menuButton = document.getElementById("menuButton");
 const menu = document.getElementById("menu");
 const overlay = document.getElementById("menuOverlay");
 
-// Open / Close Menu
-menuButton.addEventListener("click", function (e) {
+if (menuButton && menu && overlay) {
 
-    e.stopPropagation();
+    // Open / Close Menu
+    menuButton.addEventListener("click", function (e) {
 
-    menu.classList.toggle("active");
-    overlay.classList.toggle("active");
+        e.stopPropagation();
 
-});
+        menu.classList.toggle("active");
+        overlay.classList.toggle("active");
 
-// Close Menu
-overlay.addEventListener("click", function () {
+    });
 
-    menu.classList.remove("active");
-    overlay.classList.remove("active");
-
-});
-
-// Prevent Closing
-menu.addEventListener("click", function (e) {
-
-    e.stopPropagation();
-
-});
-
-// Close after selecting menu item
-const menuLinks = menu.querySelectorAll("a");
-
-menuLinks.forEach(link => {
-
-    link.addEventListener("click", () => {
+    // Close Menu
+    overlay.addEventListener("click", function () {
 
         menu.classList.remove("active");
         overlay.classList.remove("active");
 
     });
 
-});
+    // Prevent Closing
+    menu.addEventListener("click", function (e) {
+
+        e.stopPropagation();
+
+    });
+
+    // Close after selecting menu item
+    const menuLinks = menu.querySelectorAll("a");
+
+    menuLinks.forEach(link => {
+
+        link.addEventListener("click", () => {
+
+            menu.classList.remove("active");
+            overlay.classList.remove("active");
+
+        });
+
+    });
+
+}
