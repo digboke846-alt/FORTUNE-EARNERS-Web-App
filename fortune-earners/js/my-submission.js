f0efeca1effd26b8fec4b72a9487af00ad4fbf33
digboke846-alt <digboke846@gmail.com>
2026-07-14 21:15:18 +0100

    Update my-submission.js

--- fortune-earners/js/my-submission.js
+++ fortune-earners/js/my-submission.js
@@ -1,1 +1,50 @@
+import { auth, db } from "./firebase.js";
 
+import {
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+
+import {
+    collection,
+    query,
+    where,
+    getDocs,
+    orderBy
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+
+// ======================================
+// CHECK LOGIN
+// ======================================
+
+onAuthStateChanged(auth, async (user) => {
+
+    if (!user) {
+
+        window.location.href = "login.html";
+
+        return;
+
+    }
+
+    try {
+
+        const submissionList =
+            document.getElementById("submissionList");
+
+        submissionList.innerHTML =
+            "<p>Loading submissions...</p>";
+
+        // We will load the user's submissions
+        // in Part 2.
+
+    }
+
+    catch(error){
+
+        console.error(error);
+
+        alert(error.message);
+
+    }
+
+});
