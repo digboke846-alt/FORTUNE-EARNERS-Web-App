3a764c4da473d861b81b8e2903943e96e6bcdfbc
digboke846-alt <digboke846@gmail.com>
2026-07-22 21:26:39 +0100

    Update notifications.js

---
+++ fortune-earners/js/notifications.js
@@ -0,0 +1,34 @@
+import { auth, db } from "./firebase.js";
+
+import {
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+
+import {
+    collection,
+    query,
+    where,
+    orderBy,
+    getDocs,
+    updateDoc,
+    deleteDoc,
+    doc
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+
+// ======================================
+// CHECK LOGIN
+// ======================================
+
+onAuthStateChanged(auth, async (user) => {
+
+    if (!user) {
+
+        window.location.href = "login.html";
+
+        return;
+
+    }
+
+    loadNotifications(user.uid);
+
+});
