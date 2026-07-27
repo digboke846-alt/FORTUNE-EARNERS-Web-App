172582cbe3c3d342d80a4fdfc388d79a0bb1e3d4
digboke846-alt <digboke846@gmail.com>
2026-07-27 14:50:22 +0100

    Update withdraw.js

--- fortune-earners/js/withdraw.js
+++ fortune-earners/js/withdraw.js
@@ -12,7 +12,8 @@ import {
     where,
     getDocs,
     addDoc,
-    serverTimestamp
+    serverTimestamp,
+    updateDoc
 } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
 
 // ======================================
