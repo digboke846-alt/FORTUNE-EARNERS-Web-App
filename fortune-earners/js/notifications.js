86a30617c126a4505d6234ddff9d0a61c63bff50
digboke846-alt <digboke846@gmail.com>
2026-07-30 12:44:59 +0100

    Update notifications.js

--- fortune-earners/js/notifications.js
+++ fortune-earners/js/notifications.js
@@ -12,7 +12,10 @@ import {
     getDocs,
     updateDoc,
     deleteDoc,
-    doc
+    doc,
+    addDoc,
+serverTimestamp,
+    createNotification
 } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
 
 // ======================================
@@ -283,3 +286,48 @@ document
     }
 
 });
+export async function createNotification({
+
+    userId,
+
+    title,
+
+    message,
+
+    type = "General"
+
+}){
+
+    try{
+
+        await addDoc(
+
+            collection(db,"notifications"),
+
+            {
+
+                userId,
+
+                title,
+
+                message,
+
+                type,
+
+                isRead:false,
+
+                createdAt:serverTimestamp()
+
+            }
+
+        );
+
+    }
+
+    catch(error){
+
+        console.error(error);
+
+    }
+
+}
