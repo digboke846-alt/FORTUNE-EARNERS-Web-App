0205add045855935e63ca0defa38009280f6447a
digboke846-alt <digboke846@gmail.com>
2026-07-31 11:48:41 +0100

    Update referrals.js

--- fortune-earners/js/referrals.js
+++ fortune-earners/js/referrals.js
@@ -254,3 +254,20 @@ else {
     }
 
 });
+
+// ======================================
+// OPEN NOTIFICATIONS PAGE
+// ======================================
+
+const notificationButton =
+    document.getElementById("notificationButton");
+
+if (notificationButton) {
+
+    notificationButton.addEventListener("click", () => {
+
+        window.location.href = "notifications.html";
+
+    });
+
+}
